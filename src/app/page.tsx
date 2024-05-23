48f617a347b1a0c95734f7cbfaad2622711a046a
Carlos Henrique <carloshenriquepvh@hotmail.com>
2024-05-23 07:12:48 -0400

    feature/to-salvo-11 finalizando o projeto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
 import { redirect } from 'next/navigation';
+import * as React from 'react';
+import type { Metadata } from 'next';
 
-export default function Page(): never {
-  redirect('/landing-page/');
+import { Layout } from '@/components/landing-page/layout';
+import { LandingPage } from '@/components/landing-page/page';
+
+export const metadata = {
+  title: `Estou Salvo - RS`,
+  description: `Ajuda para população encontrar as pessoas nos abrigos pelo Rio grande do sul`,
+  icons: `https://estousalvo.com/logo/logo-estou-salvo.png`
+} satisfies Metadata;
+
+export default function Page(): React.JSX.Element {
+  // redirect('/landing-page/');
+  return (
+    <Layout>
+      <LandingPage />
+    </Layout>
+  )
 }
